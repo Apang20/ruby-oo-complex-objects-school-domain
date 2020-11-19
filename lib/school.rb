85ad2b101faf1fe704d7bbc71736d7fb42a89499
github username <github email address>
2020-11-18 19:32:50 -0600

    SchoolDomainLab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+    attr_reader :roster 
+
+    def initialize(school_name)
+        @school = school_name
+        @roster = {} #set it 
+    end
+
+    # def roster 
+    #     @roster
+    # end 
+
+    def add_student(name, grade)
+        if !@roster[grade] 
+        @roster[grade] = []
+        @roster[grade] << name
+        else 
+         @roster[grade] << name 
+    end
+end 
+
+    def grade(grade)
+        @roster[grade]
+    end 
+
+    def sort
+        roster.each do |grade, name|
+        roster[grade] = name.sort
+        end
+        roster 
+
+    end 
+end 
