d19d6c254af510f02915065addd88843bb13a95d
github username <github email address>
2020-11-21 15:35:25 -0600

    commit tree 1457921322f0f4fe04c0cbff16c74e8e2500e00c

--- lib/school.rb
+++ lib/school.rb
@@ -1,33 +1,37 @@
 class School
-    attr_reader :roster 
+attr_reader :roster #method to read what it is
 
-    def initialize(school_name)
-        @school = school_name
-        @roster = {} #set it 
-    end
+def initialize(school_name)
+    @school = school_name 
+    @roster = {} #method to set it to something
+end
 
-    # def roster 
-    #     @roster
-    # end 
-
-    def add_student(name, grade)
-        if !@roster[grade] 
-        @roster[grade] = []
-        @roster[grade] << name
-        else 
-         @roster[grade] << name 
-    end
+def add_student(name, grade)
+    roster[grade] ||= []
+    # ||= if truthy, move on to next line
+    # else set equal to [] then move on to next line
+    roster[grade] << name 
+    # if !@roster[grade]
+    #     @roster[grade] = []
+    #     @roster[grade] << name 
+    # else
+    #     @roster[grade] << name
+    # end
 end 
 
-    def grade(grade)
-        @roster[grade]
-    end 
+def grade(grade)
+    roster[grade]
+end 
 
-    def sort
-        roster.each do |grade, name|
-        roster[grade] = name.sort
-        end
-        roster 
+def sort
+sorted = {}
+roster.each do | grades, names|
+sorted[grades] = names.sort 
+#names.sort!
+end
+sorted
+end 
 
-    end 
+
+#@school.add_student("Homer Simpson", 9)
 end 
